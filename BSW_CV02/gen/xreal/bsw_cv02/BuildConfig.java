d82a68fe0c4340df720dca2ae4e1f1a0aeff9b56
Oscar Heredia <oheredia@xreal.me>
2014-04-22 17:16:19 -0500

    get frames, accelerometer, 5 sec record

--- BSW_CV02/gen/xreal/bsw_cv02/BuildConfig.java
+++ BSW_CV02/gen/xreal/bsw_cv02/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package xreal.BSW_CV02;
+package xreal.bsw_cv02;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
